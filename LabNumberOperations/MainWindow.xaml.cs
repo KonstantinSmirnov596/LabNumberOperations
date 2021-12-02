39d36b5d3adbedf53a579219dd59d86dd4cdcc4c
Konstantin Smirnov <smirnovko08342@gmail.com>
2021-12-02 07:36:54 +0300

    Добавьте файлы проекта.

---
+++ LabNumberOperations/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LabNumberOperations
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private int getNumber()
+        {
+            try
+            {
+                return int.Parse(TextBoxNumber.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Error!");
+                return -1;
+            }
+
+        }
+
+        private bool multipleThree(int number)
+        {
+            return number % 3 == 0;
+        }
+
+        private string isEven(int number)
+        {
+            return number % 2 == 0 ? "Even" : "Odd";
+        }
+
+        private int sumNumber(int number)
+        {
+            int sum = 0;
+            char[] arrayNumber = number.ToString().ToArray();
+            foreach (char num in arrayNumber)
+            {
+                sum += num - '0';
+            }
+            return sum;
+        }
+
+        private int multipleNumber(int number)
+        {
+            int multi = 1;
+            char[] arrayNumber = number.ToString().ToArray();
+            foreach (char num in arrayNumber)
+            {
+                multi *= num - '0';
+            }
+            return multi;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int number = getNumber();
+            if (number != -1)
+            {
+                multiThreeRes.Text = multipleThree(number).ToString();
+                evenRes.Text = isEven(number);
+                sumRes.Text = sumNumber(number).ToString();
+                multRes.Text = multipleNumber(number).ToString();
+            }
+        }
+
+        private void TextBoxNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            try
+            {
+                int number = int.Parse(textBox.Text);
+                if (textBox.Text.Length > 2)
+                {
+                    MessageBox.Show("You need to enter a two-digit number!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("You need to enter a two-digit number!");
+            }
+        }
+    }
+
+}
